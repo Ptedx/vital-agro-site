9d6487f2e724007462ff3380d51e2fc70e3283d0
vinicius <vinicosta37@gmail.com>
2024-07-29 17:27:44 -0300

    feature: added Maps api

--- src/components/body/Loc.tsx
+++ src/components/body/Loc.tsx
@@ -5,9 +5,12 @@ const Loc = ()=>{
     return(
         <LocStyle >
             <h1 id="loc">Onde nos encontrar?</h1>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Vital+Agro/@-15.9158498,-48.0515699,16z/data=!4m6!3m5!1s0x935a2d548b673ccb:0x837a1e881d5c8315!8m2!3d-15.9152551!4d-48.0506377!16s%2Fg%2F11jzwqwdrh?entry=ttu">
-                <img src="/img/localization.png" alt="Localization" width="100%" />
-            </a>
+            <iframe
+                loading="lazy"
+                allowFullScreen
+                referrerPolicy="no-referrer-when-downgrade"
+                src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJyzxni1QtWpMRFYNcHYgeeoM&key=AIzaSyB5ozTcwL2Q4Z4Vnm9qGTgpjR8j9__Icd4">
+            </iframe>
         </LocStyle>
     )
 }
--- src/styled-components/body/LocStyle.tsx
+++ src/styled-components/body/LocStyle.tsx
@@ -5,12 +5,23 @@ const LocStyle = styled.div`
     padding: 20px 0;
     text-align: center;
     border-radius: 10px;
+    width: 100%;
     h1{
         padding: 10px;
     }
     img{
         cursor: pointer;
         border-radius: 10px;
+    }
+    iframe{
+        width: 100%;
+        height: 700px;
+        border: none !important;
+    
+        &:focus, &:active, &:hover{
+            border: none;
+            outline: none;
+        }
     }
 `
 
